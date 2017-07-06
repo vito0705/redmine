1bf40a7b1d3b2fc5b91dc6e1ed5c1badd9ade7ae
vito <vito0705@163.com>
2017-07-06 10:33:46 +0800

    增加自定义标签——项目金额的状态显示功能

--- app/helpers/work_statuses_helper.rb
+++ app/helpers/work_statuses_helper.rb
@@ -1,15 +1,17 @@
 module WorkStatusesHelper
   def num_sum_percent(num, sum)
     num = num.to_f
-    sum = sum.to_i
+    sum_i = sum.to_i
+    sum_f = sum.to_f
     # p num
     # p sum
-    if sum == 0
+    if sum_i == 0
       return "----"
     else
-      value = (num / sum)
+      value = (num / sum_f)
       value = (value * 100).round(2).to_s + "%"
       return value
     end
   end
+
 end
--- app/models/work_status.rb
+++ app/models/work_status.rb
@@ -1,23 +1,32 @@
 class WorkStatus < ActiveRecord::Base
   def self.status_info_json user_id
+    customized = IssueCustomField.where("name = ?","项目金额").first
     work_info = Issue.where("assigned_to_id = ?" , user_id).order(:status_id).all
     render_user_hash = Hash.new
 
     status = IssueStatus.all
     status.each do |sta|
       work_num = 0
+      money_num = 0.0
       render_status_hash = Hash.new
 
       record = Task.find_or_create_record(user_id, sta[:id])
       record_hash = record.attributes.dup
-      #计算各个状态下的工作总量
+      #计算各个状态下的工作总量和任务金额总量
       work_info.each do |work|
         if work[:status_id] == sta[:id]
           work_num = work_num + 1
+          money_task_str = work.custom_field_value customized[:id]
+          money_task = money_task_str.to_f
+          if money_task.nil? || money_task.blank?
+            money_task = 0.0
+          end
+          money_num = money_num + money_task
         end
       end
       render_status_hash[:status] = sta.attributes.dup
       render_status_hash[:num] = work_num
+      render_status_hash[:money] = money_num
       render_status_hash[:record] = record_hash
 
       render_user_hash["#{sta["name"]}"] = render_status_hash
